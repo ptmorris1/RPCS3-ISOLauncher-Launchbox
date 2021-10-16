57f3b5713d9bec01ec4a80efebc4f6d7c690c77b
PM <phunkypm@gmail.com>
2021-10-16 05:18:19 -0500

    Updated to use Powershell as emulator in Launchbox

--- RPCS3-ISO-LaunchBox.ps1
+++ RPCS3-ISO-LaunchBox.ps1
@@ -1,35 +1,24 @@
-﻿# Needs rpcs3.exe path and ISO path as parameters.
+﻿# Will get ISO param from Launchbox
 param (
     [Parameter(Mandatory=$true)]
-    [string]$RPCS3path,
-
-    [Parameter(Mandatory=$true)]
-    [string]$ISOpath,
-    [Int32]$Seconds = '10'
+    [string]$ISOpath
 )
 
+# Need to set path to rpcs3.exe file
+$RPCS3path = "E:\LaunchBox\Emulators\rpcs3\rpcs3.exe"
 # Mounts the ISO to a drive letter
 $Vol = Mount-DiskImage -ImagePath $ISOpath -PassThru
+# sleeps to wait for disk to mount and obtain drive letter.
+Start-Sleep -Seconds 2
 # Then gets the drive letter that the ISO was mounted to.
-$Vol = $vol | Get-Volume
+$Voldisk = $vol | Get-Volume
 # Created the $path variable to the EBOOT.BIN file for lanching in rpcs3
-$path = $vol.DriveLetter + ":\PS3_GAME\USRDIR\EBOOT.BIN"
-# Lanches rpcs3 with EBOOT.BIN path
+$path = $Voldisk.DriveLetter + ":\PS3_GAME\USRDIR\EBOOT.BIN"
+# Lanches rpcs3 with EBOOT.BIN path, can add extra arguments for rpcs3.exe if needed.  Not tested.
 & $RPCS3path $path
-# sleeps until process has a chance to start
-Start-Sleep -Seconds $Seconds
-# Gets the rpcs3 process details
-$Process = Get-Process rpcs3
-    
-    # Waits for the rpcs3 process to end before script continues.
-    # Using While since script now keep rpcs3 process open even after GUI is closed
-    While ($Process.MainWindowHandle -ne 0) {
-        #checks ever second if process mainwindowhandle is 0
-        Start-Sleep -Seconds 1
-        $Process = Get-Process rpcs3
-        }
-
-    # Kills rpcs3 left open by script
-    $Process | Stop-Process
-    # Dismounts the ISO image drive
-    Dismount-DiskImage -ImagePath $ISOpath
+# sleeps until rpcs3 has a chance to start
+Start-Sleep -Seconds 2
+# Waits for rpcs3 to be closed
+Wait-Process rpcs3
+# Dismounts the ISO image drive
+Dismount-DiskImage -ImagePath $ISOpath
