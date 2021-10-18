68d597661acfe1634cb12cc21cd9d417957be6a5
PM <phunkypm@gmail.com>
2021-10-18 06:45:43 -0500

    updated sscript

--- RPCS3-ISO-LaunchBox.ps1
+++ RPCS3-ISO-LaunchBox.ps1
@@ -4,19 +4,19 @@ param (
     [string]$ISOpath
 )
 
-# Need to set path to rpcs3.exe file
-$RPCS3path = "E:\LaunchBox\Emulators\rpcs3\rpcs3.exe"
+# Put script in same folder as rpcs3.exe or change to direct path.
+$RPCS3path = "$PSScriptRoot\rpcs3.exe"
 # Mounts the ISO to a drive letter
 $Vol = Mount-DiskImage -ImagePath $ISOpath -PassThru
-# sleeps to wait for disk to mount and obtain drive letter.
+# sleeps 2 seconds to wait for disk to mount.  Can be changed as needed.
 Start-Sleep -Seconds 2
 # Then gets the drive letter that the ISO was mounted to.
 $Voldisk = $vol | Get-Volume
 # Created the $path variable to the EBOOT.BIN file for lanching in rpcs3
 $path = $Voldisk.DriveLetter + ":\PS3_GAME\USRDIR\EBOOT.BIN"
-# Lanches rpcs3 with EBOOT.BIN path, can add extra arguments for rpcs3.exe if needed.  Not tested.
+# Lanches rpcs3 with EBOOT.BIN path, can add extra arguments for rpcs3.exe if needed.  Not tested in this script.
 & $RPCS3path $path
-# sleeps until rpcs3 has a chance to start
+# sleeps 2 seconds until rpcs3 has a chance to start
 Start-Sleep -Seconds 2
 # Waits for rpcs3 to be closed
 Wait-Process rpcs3
