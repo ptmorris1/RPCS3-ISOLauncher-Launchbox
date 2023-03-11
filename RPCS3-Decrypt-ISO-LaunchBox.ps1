a50273adc2650b467e453695f2573acdbc742af4
PM <phunkypm@gmail.com>
2023-03-11 13:47:10 -0600

    cleaned up some code

--- RPCS3-Decrypt-ISO-LaunchBox.ps1
+++ RPCS3-Decrypt-ISO-LaunchBox.ps1
@@ -10,59 +10,35 @@ $RPCS3path = "$PSScriptRoot\rpcs3.exe"
 $PS3DEC = "$PSScriptRoot\ps3dec\PS3Dec.exe"
 #path to disc key files for decryptions
 $KeysPath = "$PSScriptRoot\dkeys"
-#leave as '' or change to path without trailing \
+#leave as '' or change to path without trailing \  for example F: or F:\myisos
 $DecryptPath = ''
 
+$Keys = Get-ChildItem -LiteralPath $KeysPath
+$ISOname = [System.IO.Path]::GetFileNameWithoutExtension($ISOpath)
+$Key = $Keys | Where-Object -Property name -Like "$ISOname*"
+$KeyValue = Get-Content -LiteralPath $key.FullName
 if ([string]::IsNullOrWhiteSpace($DecryptPath)) {
-    $Keys = Get-ChildItem -LiteralPath $KeysPath
-    $ISOname = [System.IO.Path]::GetFileNameWithoutExtension($ISOpath)
-    $Key = $Keys | Where-Object -Property name -Like "$ISOname*"
-    $KeyValue = Get-Content -LiteralPath $key.FullName
     $DecryptISO = (Split-Path $ISOpath -Parent) + "\$ISOname" + '_DEC.iso'
-
-    & $PS3DEC d key $KeyValue $ISOpath $DecryptISO | Out-Null
-
-    # Mounts the ISO to a drive letter
-    $Vol = Mount-DiskImage -ImagePath $DecryptISO -PassThru
-    # sleeps 2 seconds to wait for disk to mount.  Can be changed as needed.
-    Start-Sleep -Seconds 2
-    # Then gets the drive letter that the ISO was mounted to.
-    $Voldisk = $vol | Get-Volume
-    # Created the $path variable to the EBOOT.BIN file for lanching in rpcs3
-    $path = $Voldisk.DriveLetter + ':\PS3_GAME\USRDIR\EBOOT.BIN'
-    # Lanches rpcs3 with EBOOT.BIN path, can add extra arguments for rpcs3.exe if needed.  Not tested in this script.
-    & $RPCS3path $path
-    # sleeps 2 seconds until rpcs3 has a chance to start
-    Start-Sleep -Seconds 2
-    # Waits for rpcs3 to be closed
-    Wait-Process rpcs3
-    # Dismounts the ISO image drive
-    Dismount-DiskImage -ImagePath $DecryptISO
-    Remove-Item -LiteralPath $DecryptISO -Force
 } else {
-    $Keys = Get-ChildItem -LiteralPath $KeysPath
-    $ISOname = [System.IO.Path]::GetFileNameWithoutExtension($ISOpath)
-    $Key = $Keys | Where-Object -Property name -Like "$ISOname*"
-    $KeyValue = Get-Content -LiteralPath $key.FullName
     $DecryptISO = $DecryptPath + "\$ISOname" + '_DEC.iso'
-
-    & $PS3DEC d key $KeyValue $ISOpath $DecryptISO | Out-Null
-
-    # Mounts the ISO to a drive letter
-    $Vol = Mount-DiskImage -ImagePath $DecryptISO -PassThru
-    # sleeps 2 seconds to wait for disk to mount.  Can be changed as needed.
-    Start-Sleep -Seconds 2
-    # Then gets the drive letter that the ISO was mounted to.
-    $Voldisk = $vol | Get-Volume
-    # Created the $path variable to the EBOOT.BIN file for lanching in rpcs3
-    $path = $Voldisk.DriveLetter + ':\PS3_GAME\USRDIR\EBOOT.BIN'
-    # Lanches rpcs3 with EBOOT.BIN path, can add extra arguments for rpcs3.exe if needed.  Not tested in this script.
-    & $RPCS3path $path
-    # sleeps 2 seconds until rpcs3 has a chance to start
-    Start-Sleep -Seconds 2
-    # Waits for rpcs3 to be closed
-    Wait-Process rpcs3
-    # Dismounts the ISO image drive
-    Dismount-DiskImage -ImagePath $DecryptISO
-    Remove-Item -LiteralPath $DecryptISO -Force
 }
+
+& $PS3DEC d key $KeyValue $ISOpath $DecryptISO | Out-Null
+
+# Mounts the ISO to a drive letter
+$Vol = Mount-DiskImage -ImagePath $DecryptISO -PassThru
+# sleeps 2 seconds to wait for disk to mount.  Can be changed as needed.
+Start-Sleep -Seconds 2
+# Then gets the drive letter that the ISO was mounted to.
+$Voldisk = $vol | Get-Volume
+# Created the $path variable to the EBOOT.BIN file for lanching in rpcs3
+$path = $Voldisk.DriveLetter + ':\PS3_GAME\USRDIR\EBOOT.BIN'
+# Lanches rpcs3 with EBOOT.BIN path, can add extra arguments for rpcs3.exe if needed.  Not tested in this script.
+& $RPCS3path $path
+# sleeps 2 seconds until rpcs3 has a chance to start
+Start-Sleep -Seconds 2
+# Waits for rpcs3 to be closed
+Wait-Process rpcs3
+# Dismounts the ISO image drive
+Dismount-DiskImage -ImagePath $DecryptISO
+Remove-Item -LiteralPath $DecryptISO -Force
